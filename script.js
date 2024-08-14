b68da39e1b4b5f6f46f3dcdaa56265ab1bb335ec
Ytikare <ytikare2@tutanota.com>
2024-08-14 17:05:35 +0300

    Add events to number and operand buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const numberButtonsContainer = document.querySelector('.number-buttons');
+const operandButtons = document.querySelectorAll('.operand-buttons > .row > .operand');
+const calcDisplay = document.querySelector('.display > input');
+const solveButton = document.getElementById('=');
+
+numberButtonsContainer.addEventListener('click', e => {
+    let target = e.target;
+
+    if (target.localName == 'button') {
+        calcDisplay.value += target.id;
+    }
+});
+
+operandButtons.forEach(el => {
+    el.addEventListener('click' , e => {
+        calcDisplay.value += e.target.id
+    })
+});
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -38,3 +57,6 @@ function operate(operatorOne, operatorTwo, operand) {
 
     return result;
 }
+
+
+
